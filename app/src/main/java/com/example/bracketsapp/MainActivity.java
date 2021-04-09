443013aadc147ea042eadfd5d737493d5011e939
fernandsboy <lucasfelippazzi@gmail.com>
2021-04-09 15:44:22 -0300

    botoes puxam proximas telas

--- app/src/main/java/com/example/bracketsapp/MainActivity.java
+++ app/src/main/java/com/example/bracketsapp/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.bracketsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoCadastrar, botaoLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoCadastrar = (Button) findViewById(R.id.bt_criar);
+        botaoLogin = (Button) findViewById(R.id.bt_login);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), ActivityCriar.class);
+                startActivity(intent);
+            }
+        });
+
+        botaoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
