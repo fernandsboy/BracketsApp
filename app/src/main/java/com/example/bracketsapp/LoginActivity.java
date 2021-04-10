6b93697a0c8cd3bc580f7b7e3c134afb4f614fdd
fernandsboy <lucasfelippazzi@gmail.com>
2021-04-09 22:47:16 -0300

    campos de textos estão sendo salvos e volta mensagem de confirmação

--- app/src/main/java/com/example/bracketsapp/LoginActivity.java
+++ app/src/main/java/com/example/bracketsapp/LoginActivity.java
@@ -3,12 +3,38 @@ package com.example.bracketsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, senha;
+    private Button botaoEnviarLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.id_EmailLogin);
+        senha = findViewById(R.id.id_SenhaLogin);
+
+        botaoEnviarLogin = (Button) findViewById(R.id.bt_EnviarLogin);
+
+        botaoEnviarLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String editEmail = email.getText().toString();
+                String editSenha = senha.getText().toString();
+
+                if (editEmail.isEmpty() || editSenha.isEmpty()){
+                    Snackbar.make(v,"Preencha todos os campos!!", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
