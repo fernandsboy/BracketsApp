7ee15c7a4c8dd62c470c1c99aa0908798c9bfcc6
fernandsboy <lucasfelippazzi@gmail.com>
2021-04-10 17:30:23 -0300

    pronto

--- app/src/main/java/com/example/bracketsapp/ActivityCriar.java
+++ app/src/main/java/com/example/bracketsapp/ActivityCriar.java
@@ -1,20 +1,42 @@
 package com.example.bracketsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivityCriar extends AppCompatActivity {
 
     private EditText nome, email, telefone, senha;
     private TextView resultadoNome, resultadoEmail, resultadoTelefone, resultadoSenha;
     private Button botaoEnviarCadastro;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore fStore;
+    private String userID;
+
+   private String editNome ;
+  private   String editEmail ;
+  private   String editTelefone ;
+   private String editSenha ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +51,8 @@ public class ActivityCriar extends AppCompatActivity {
         resultadoEmail = findViewById(R.id.id_resultadoEmail);
         resultadoTelefone = findViewById(R.id.id_resultadoTelefone);
         resultadoSenha = findViewById(R.id.id_resultadoSenha);
+        mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         botaoEnviarCadastro = (Button) findViewById(R.id.bt_EnviarCadastro);
 
@@ -36,19 +60,57 @@ public class ActivityCriar extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String editNome = nome.getText().toString();
-                String editEmail = email.getText().toString();
-                String editTelefone = telefone.getText().toString();
-                String editSenha = senha.getText().toString();
+               editNome = nome.getText().toString();
+               editEmail = email.getText().toString();
+                editTelefone = telefone.getText().toString();
+                editSenha = senha.getText().toString();
 
                 if (editNome.isEmpty() || editEmail.isEmpty() || editTelefone.isEmpty() || editSenha.isEmpty()){
                     Snackbar.make(v,"Preencha todos os campos!!", Snackbar.LENGTH_LONG).show();
                 }else{
-                    resultadoNome.setText(editNome);
-                    resultadoEmail.setText(editEmail);
-                    resultadoTelefone.setText(editTelefone);
-                    resultadoSenha.setText(editSenha);
-                    Snackbar.make(v,"Cadastro feito com sucesso!!", Snackbar.LENGTH_LONG).show();
+
+
+                    mAuth.createUserWithEmailAndPassword(editEmail , editSenha)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if(task.isSuccessful())
+                                    {
+                                        userID = mAuth.getCurrentUser().getUid();
+                                        DocumentReference documentReference = fStore.collection("users")
+                                                .document(userID);
+
+                                        Map<String, Object> user = new HashMap<>();
+                                        user.put("name" , editNome);
+                                        user.put("email", editEmail);
+                                        user.put("number", editTelefone);
+                                        user.put("password", editSenha);
+                                        documentReference.set(user)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Toast.makeText(ActivityCriar.this, "Banco de dados salvo", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+
+                                                Toast.makeText(ActivityCriar.this, ""+e, Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                        Intent intent = new Intent(getApplicationContext() , LoginActivity.class);
+                                        startActivity(intent);
+                                        Toast.makeText(ActivityCriar.this, "Conta criada", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(ActivityCriar.this, ""+e, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
                 }
 
             }
--- app/src/main/java/com/example/bracketsapp/LoginActivity.java
+++ app/src/main/java/com/example/bracketsapp/LoginActivity.java
@@ -1,18 +1,27 @@
 package com.example.bracketsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText email, senha;
     private Button botaoEnviarLogin;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,7 @@ public class LoginActivity extends AppCompatActivity {
 
         email = findViewById(R.id.id_EmailLogin);
         senha = findViewById(R.id.id_SenhaLogin);
+        mAuth = FirebaseAuth.getInstance();
 
         botaoEnviarLogin = (Button) findViewById(R.id.bt_EnviarLogin);
 
@@ -33,6 +43,29 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (editEmail.isEmpty() || editSenha.isEmpty()){
                     Snackbar.make(v,"Preencha todos os campos!!", Snackbar.LENGTH_LONG).show();
+                }
+                else
+                {
+
+                    mAuth.signInWithEmailAndPassword(editEmail ,editSenha)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if(task.isSuccessful())
+                                    {
+                                        Toast.makeText(LoginActivity.this, "login feito com sucesso", Toast.LENGTH_SHORT).show();
+
+
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            Toast.makeText(LoginActivity.this, ""+e, Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
             }
         });
