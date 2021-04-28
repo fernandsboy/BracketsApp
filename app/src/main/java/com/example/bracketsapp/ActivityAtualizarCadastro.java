a2b2e4bc04feb51bd2aa941aa83fb5c8a2f6c50a
fernandsboy <lucasfelippazzi@gmail.com>
2021-04-28 16:52:39 -0300

    Tarefa 3 tela de atualizar cadastro

---
+++ app/src/main/java/com/example/bracketsapp/ActivityAtualizarCadastro.java
@@ -0,0 +1,14 @@
+package com.example.bracketsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAtualizarCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atualizar_cadastro);
+    }
+}
